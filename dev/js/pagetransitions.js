3375e71f41da4d27925ca551702db42cd432ab0f
ggmanuel <mgarcia@makinsense.com>
2016-11-16 17:10:40 -0300

    * mobile support
    * pulsable events

--- dev/js/pagetransitions.js
+++ dev/js/pagetransitions.js
@@ -1,10 +1,19 @@
+window.onload = function(){
+	if ( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+		$('source').empty();
+	} else {
+		$('img').empty();
+	}
+}
+
 var PageTransitions = (function() {
 
 	var $main = $( '#pt-main' ),
 		$pages = $main.children( 'div.pt-page' ),
 		$iterate = $( '.iterateEffects' ),
-		$pulsable = $( '.pulsable' ),
+		$pulsable = $('.pulsable')
 		$email = $('#email-input'),
+		$form = $('#email-form'),
 		current = 'pt-page-home',
 		isAnimating = false,
 		endCurrPage = false,
@@ -21,44 +30,50 @@ var PageTransitions = (function() {
 		support = Modernizr.cssanimations;
 
 	function init() {
-
-		$pages.each( function() {
+		$pages.each(function() {
 			var $page = $( this );
 			$page.data( 'originalClassList', $page.attr( 'class' ) );
-		} );
+		});
 
 		$pages.filter('.' + current).addClass( 'pt-page-current active' ).find('video')[0].play();
 
-		$iterate.on( 'click', function() {
+		$pulsable.on('webkitAnimationEnd oanimationend msAnimationEnd animationend', function() {
+			$(this).addClass('pulse');
+		});
+
+		$iterate.on('click', function() {
 			var options = {};
 
-			if( isAnimating ) {
+			if (isAnimating) {
 				return false;
 			}
+
 			options.animation = $(this).attr('data-direction');
 			options.showPage = $(this).attr('data-target');
 			nextPage( options );
-		} );
+		});
 
-		$pulsable.on( 'click', function() {
-			$(this).addClass('pulse');
-		} );
+		$form.submit( function() {
+			acceptEmail();
+			return false;
+		});
 
-		$email.on( 'invalid', function() {
+		$email.on('invalid', function() {
 			if ($email[0].validity.typeMismatch || $email[0].validity.valueMissing) {
 				$email[0].setCustomValidity(" ");
 				$email.parent().addClass('error');
 		  } else {
 				$email.parent().removeClass('error');
+				acceptEmail();
 		  }
-		} );
+		});
 
 	}
 
 	function nextPage( options ) {
 		var animation = options.animation;
 
-		if( isAnimating ) {
+		if (isAnimating) {
 			return false;
 		}
 
@@ -77,8 +92,7 @@ var PageTransitions = (function() {
 			current = 'pt-page-6';
 		}
 
-		switch( animation ) {
-
+		switch(animation) {
 			case 'left':
 				outClass = 'pt-page-moveToLeft active-out';
 				inClass = 'pt-page-moveFromRight';
@@ -89,44 +103,52 @@ var PageTransitions = (function() {
 				break;
 			case 'up':
 				outClass = 'pt-page-moveToTop active-out';
-				inClass = 'pt-page-moveFromBottom';
+				inClass = 'pt-page-moveFromBottom .pt-page-delay700';
 				break;
 			case 'down':
 				outClass = 'pt-page-moveToBottom active-out';
 				inClass = 'pt-page-moveFromTop';
 				break;
-
 		}
 
-		$currPage.addClass( outClass ).on( animEndEventName, function() {
-			$currPage.off( animEndEventName );
+		$currPage.addClass(outClass).on(animEndEventName, function() {
+			$currPage.off(animEndEventName);
 			endCurrPage = true;
-			if( endNextPage ) {
+			if (endNextPage) {
 				onEndAnimation( $currPage, $nextPage );
 			}
-		} );
+		});
 
-		$nextPage.addClass( inClass ).on( animEndEventName, function() {
-			$nextPage.off( animEndEventName );
+		$nextPage.addClass(inClass).on(animEndEventName, function() {
+			$nextPage.off(animEndEventName);
 			$nextPage.find('video')[0].play()
 			endNextPage = true;
-			if( endCurrPage ) {
+			if (endCurrPage) {
 				onEndAnimation( $currPage, $nextPage );
 			}
-		} );
+		});
 	}
 
-	function onEndAnimation( $outpage, $inpage ) {
+	function onEndAnimation($outpage, $inpage) {
 		endCurrPage = false;
 		endNextPage = false;
 		resetPage( $outpage, $inpage );
 		isAnimating = false;
 	}
 
-	function resetPage( $outpage, $inpage ) {
+	function resetPage($outpage, $inpage) {
 		$outpage.attr( 'class', $outpage.data( 'originalClassList' ) );
 		$inpage.attr( 'class', $inpage.data( 'originalClassList' ) + ' pt-page-current active' );
 		$outpage.find('video')[0].pause()
+	}
+
+	function acceptEmail() {
+		$form.addClass('success');
+		$email.val('');
+
+		setTimeout(function(){
+			$form.removeClass('success');
+		}, 3000);
 	}
 
 	init();
